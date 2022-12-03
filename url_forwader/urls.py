9002f94eca715a07ea4a0fc328e9982bc9478b73
sheggamharshith <harshit675@gmail.com>
2022-12-02 23:44:40 -0500

    initial

--- url_forwader/urls.py
+++ url_forwader/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from .views import redirect_view
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("",redirect_view,name="redirect")
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
